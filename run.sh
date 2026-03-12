0179087fedcf35f0b6c54adc5758d10589cefaff
ChenxuKwok <chenxu.guo.work@outlook.com>
2026-03-12 20:47:06 +0800

    removed unnecessary scripts

--- run.sh
+++
@@ -1,6 +0,0 @@
-python your_script.py \
-  --input_jsonl data/en_data.jsonl \
-  --output_jsonl data/en_processed.jsonl \
-  --error_jsonl data/en_processed.errors.jsonl \
-  --gpus 0,1 \
-  --pr_batch_size 16
